ae5c5fdfa2f7657caa1fa537c0a92216bf4f11a0
chasulim <ggggggggg1948@gmail.com>
2024-12-01 00:27:41 +0900

    보드 페이지 리스폰스 안드로이드와 형식 맞춰줌

--- src/main/java/com/busanit501/springproject3/msy/dto/BoardPageResponse.java
+++ src/main/java/com/busanit501/springproject3/msy/dto/BoardPageResponse.java
@@ -1,5 +1,6 @@
 package com.busanit501.springproject3.msy.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class BoardPageResponse {
+    @JsonProperty("content") // JSON 응답의 필드 이름을 "content"로 설정
     private List<BoardDto> boards;
     private int totalPages;
     private long totalElements;
